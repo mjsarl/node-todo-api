d4829ee3b98ff99755e07fd2c3355755466d9a91
Mark Sarl <mark.sarl@gmail.com>
2017-07-26 12:25:22 +0100

    Added PATCH route testing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'First thing to do'
 },{
   _id: new ObjectID(),
-  text: 'Second thing to do'
+  text: 'Second thing to do',
+  completed: true,
+  completedAt: 13456
 },{
   _id: new ObjectID(),
   text: 'Third thing to do'
@@ -76,7 +78,7 @@ describe('GET /todos', ()=>{
     })
     //id = res.body.todos[0]._id;
     .end(done);
-  })
+  });
   it('Should get a todo with specified ID', (done)=>{
     request(app)
     .get(`/todos/${todos[0]._id.toHexString()}`)
@@ -138,7 +140,39 @@ describe('DELETE /todos:id', ()=>{
     .expect(404)
     .end(done);
   });
+});
 
+describe('PATCH /todos:id', ()=>{
+  it('Should update text and change completed from false to true', (done)=>{
+    var HexID = todos[0]._id.toHexString();
+    var newText = 'Test Text Content';
+    request(app)
+    .patch(`/todos/${HexID}`)
+    .send({text: newText, completed:true})
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todo._id).toBe(HexID);
+      expect(res.body.todo.text).toBe(newText);
+      expect(res.body.todo.completed).toBe(true);
+      expect(res.body.todo.completedAt).toBeA('number');
+    })
+    .end(done);
+  });
 
+  it('Should set completedAt to null when you change completed from True to false', (done)=>{
+    var HexID = todos[1]._id.toHexString();
+    var newText2 = 'Second Test Text Content';
+    request(app)
+    .patch(`/todos/${HexID}`)
+    .send({text: newText2, completed:false})
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todo._id).toBe(HexID);
+      expect(res.body.todo.text).toBe(newText2);
+      expect(res.body.todo.completed).toBe(false);
+      expect(res.body.todo.completedAt).toNotExist();
+    })
+    .end(done);
+  });
 
 });
