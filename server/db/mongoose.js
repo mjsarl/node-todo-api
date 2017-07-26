ee9b8ede896a3cdc564c123fa56b89f2f8607d29
Mark Sarl <mark.sarl@gmail.com>
2017-07-26 18:35:37 +0100

    Setup ENV variable config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require ('mongoose');
 
 // const DBConnStr = 'mongodb://1.20.30.78:27017/TodoApp';
-const DBConnStr = 'mongodb://localhost:27017/TodoApp';
+// const DBConnStr = 'mongodb://localhost:27017/TodoApp';
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || DBConnStr);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
