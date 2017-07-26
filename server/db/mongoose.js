4332f2aba0582764b3ac464da1b53fcc0f8cda47
Mark Sarl <mark.sarl@gmail.com>
2017-07-26 10:26:02 +0100

    Added PATCH route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require ('mongoose');
 
-const DBConnStr = 'mongodb://1.20.30.78:27017/TodoApp';
+// const DBConnStr = 'mongodb://1.20.30.78:27017/TodoApp';
+const DBConnStr = 'mongodb://localhost:27017/TodoApp';
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || DBConnStr);
 
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
-var express = require ('express');
-var bodyParser = require('body-parser');
-
-var {mongoose} = require('./db/mongoose');
+const _ = require ('lodash');
+const express = require ('express');
+const bodyParser = require('body-parser');
+const {mongoose} = require('./db/mongoose');
 const {ObjectID} = require('mongodb');
+
+
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
 
@@ -77,6 +79,34 @@ app.delete('/todos/:id',(req,res)=>{
     res.status(400).send();
   });
 });
+
+app.patch('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)){
+      return res.status(404).send();
+    }
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (_.isBoolean(body.completed) && body.completed){
+      //completed is true
+      body.completedAt = new Date().getTime();
+
+    } else {
+      body.completed = false;
+      body.completedAt = null;
+
+    }
+
+    Todo.findByIdAndUpdate(id, {$set:body}, {new: true}).then((todo)=>{
+      if (!todo){
+        return res.status(404).send();
+      }
+      return res.status(200).send({todo});
+    }).catch((e)=> {
+      res.status(400).send();
+    });
+});
+
 
 module.exports = {app};
 
