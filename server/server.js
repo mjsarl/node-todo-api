7899117a5080caf913205225aff492656eca1ddc
Mark <mark.sarl@gmail.com>
2017-08-20 21:24:30 +0100

    Added Async Await usage

--- server/server.js
+++ server/server.js
@@ -63,23 +63,39 @@ Todo.findOne({
 
 });
 
-app.delete('/todos/:id', authenticate, (req,res)=>{
-  var id = req.params.id;
-  if(!ObjectID.isValid(id)){
+app.delete('/todos/:id', authenticate, async (req,res)=>{
+  try {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+      throw new Error;
+    }
+    const todo = await Todo.findOneAndRemove({
+      _id: id,
+      _creator: req.user._id
+    });
+    if(!todo){
+      throw new Error;
+    }
+    res.send({todo});
+  } catch (e) {
     return res.status(404).send();
   }
-
-  Todo.findOneAndRemove({
-    _id: id,
-    _creator: req.user._id
-  }).then((todo)=>{
-      if(!todo){
-        return res.status(404).send();
-      }
-      res.send({todo});
-  }).catch((e)=>{
-    res.status(400).send();
-  });
+  // var id = req.params.id;
+  // if(!ObjectID.isValid(id)){
+  //   return res.status(404).send();
+  // }
+  //
+  // Todo.findOneAndRemove({
+  //   _id: id,
+  //   _creator: req.user._id
+  // }).then((todo)=>{
+  //     if(!todo){
+  //       return res.status(404).send();
+  //     }
+  //     res.send({todo});
+  // }).catch((e)=>{
+  //   res.status(400).send();
+  // });
 });
 
 app.patch('/todos/:id', authenticate, (req, res)=>{
@@ -112,17 +128,26 @@ app.patch('/todos/:id', authenticate, (req, res)=>{
     });
 });
 
-app.post('/users', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  var user = new User (body);
-  user.save().then(()=>{
-    // res.status(200).send(doc);
-    return user.generateAuthToken();
-  }).then((token)=>{
+app.post('/users', async (req, res) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = new User (body);
+    await user.save();
+    const token = await user.generateAuthToken();
     res.header('x-auth', token).send(user);
-  }).catch((e)=>{
+  } catch (e) {
     res.status(400).send(e.errmsg || e);
-  });
+  }
+  // var body = _.pick(req.body, ['email', 'password']);
+  // var user = new User (body);
+  // user.save().then(()=>{
+  //   // res.status(200).send(doc);
+  //   return user.generateAuthToken();
+  // }).then((token)=>{
+  //   res.header('x-auth', token).send(user);
+  // }).catch((e)=>{
+  //   res.status(400).send(e.errmsg || e);
+  // });
 });
 
 
@@ -132,27 +157,34 @@ app.get('/users/me', authenticate, (req, res) => {
 
 });
 
-app.post('/users/login', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  var email = body.email;
-  var password = body.password;
-
-  User.findByCredentials(email, password).then((user)=>{
-    // console.log('USER returned to server.js = ', user.email);
-    return user.generateAuthToken().then((token)=>{
-      res.header('x-auth', token).send(user);
-    });
-  }).catch((e)=>{
+app.post('/users/login', async (req, res) => {
+  try {
+    var body = _.pick(req.body, ['email', 'password']);
+    var email = body.email;
+    var password = body.password;
+    const user = await User.findByCredentials(email, password);
+    const token = await user.generateAuthToken();
+    res.header('x-auth', token).send(user);
+  } catch (e) {
     res.status(400).send(e);
-  });
+  }
+  // User.findByCredentials(email, password).then((user)=>{
+  //   // console.log('USER returned to server.js = ', user.email);
+  //   return user.generateAuthToken().then((token)=>{
+  //     res.header('x-auth', token).send(user);
+  //   });
+  // }).catch((e)=>{
+  //   res.status(400).send(e);
+  // });
 });
 
-app.delete('/users/me/token', authenticate, (req,res)=>{
-    req.user.removeToken(req.token).then(()=>{
+app.delete('/users/me/token', authenticate, async (req,res)=>{
+  try {
+    await req.user.removeToken(req.token);
     res.status(200).send();
-  }, ()=>{
+  } catch (e) {
     res.status(400).send();
-  });
+  }
 });
 
 
